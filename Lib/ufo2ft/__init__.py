3153bbd57fcca63353a23176e5e847c209e3dbde
James Godfrey-Kittle <jamesgk@google.com>
2016-02-02 14:51:40 -0800

    Revert "Add option to use FontTools' mtiLib"
    This reverts commit d6172966fbd902ceba7523a3e804e74eb2495926.

--- Lib/ufo2ft/__init__.py
+++ Lib/ufo2ft/__init__.py
@@ -7,32 +7,32 @@ from ufo2ft.outlineOTF import OutlineOTFCompiler, OutlineTTFCompiler
 
 
 def _compile(font, glyphOrder, outlineCompilerClass, featureCompilerClass,
-             mtiFeaFiles, kernWriter, markWriter):
+             kernWriter, markWriter):
     """Create FontTools TTFonts from a UFO."""
 
     outlineCompiler = outlineCompilerClass(font, glyphOrder=glyphOrder)
     outline = outlineCompiler.compile()
 
     featureCompiler = featureCompilerClass(
-        font, outline, kernWriter, markWriter, mtiFeaFiles=mtiFeaFiles)
+        font, outline, kernWriter=kernWriter, markWriter=markWriter)
     featureCompiler.compile()
 
     return outline
 
 
 def compileOTF(font, glyphOrder=None, outlineCompilerClass=OutlineOTFCompiler,
-               featureCompilerClass=FeatureOTFCompiler, mtiFeaFiles=None,
+               featureCompilerClass=FeatureOTFCompiler,
                kernWriter=KernFeatureWriter, markWriter=MarkFeatureWriter):
     """Create FontTools CFF font from a UFO."""
 
     return _compile(font, glyphOrder, outlineCompilerClass,
-                    featureCompilerClass, mtiFeaFiles, kernWriter, markWriter)
+                    featureCompilerClass, kernWriter, markWriter)
 
 
 def compileTTF(font, glyphOrder=None, outlineCompilerClass=OutlineTTFCompiler,
-               featureCompilerClass=FeatureOTFCompiler, mtiFeaFiles=None,
+               featureCompilerClass=FeatureOTFCompiler,
                kernWriter=KernFeatureWriter, markWriter=MarkFeatureWriter):
     """Create FontTools TrueType font from a UFO."""
 
     return _compile(font, glyphOrder, outlineCompilerClass,
-                    featureCompilerClass, mtiFeaFiles, kernWriter, markWriter)
+                    featureCompilerClass, kernWriter, markWriter)
